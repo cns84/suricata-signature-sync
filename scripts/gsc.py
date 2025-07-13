fe9ed1cfb8a8452ad2873f178730ee6abefbe75f
cns84 <cns_84@outlook.com>
2025-07-13 16:54:01 -0400

    Update gsc.py

--- scripts/gsc.py
+++ scripts/gsc.py
@@ -4,13 +4,13 @@ vars:
   address-groups:
     HOME_NET: "[192.168.0.0/16]"
     EXTERNAL_NET: "[8.8.8.8, 1.1.1.1]"
-    DNS_SERVERS: "[192.168.0.53]"
+    DNS_SERVERS: "[192.168.1.6]"
     SMTP_SERVERS: "[192.168.0.25]"
     HTTP_SERVERS: "[192.168.0.80]"
     SQL_SERVERS: "[192.168.0.143]"
     SHELLCODE_SERVERS: "[192.168.0.200]"
-    SIP_SERVERS: "[192.168.0.5060]"
-    AIM_SERVERS: "[192.168.0.4099]"
+    SIP_SERVERS: "[192.168.0.1]"
+    AIM_SERVERS: "[192.168.0.1]"
 default-rule-path: rules
 default-log-dir: /tmp/suricata-logs
 """
